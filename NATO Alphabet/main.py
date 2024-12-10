584ce33c1782338a84a34e59898e235d47096235
Lawrence <lawrence891106@gmail.com>
2024-12-10 21:26:40 +0800

    Update the alphabet project

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -8,7 +8,12 @@ alphabet_file = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter:row.code for (index, row) in alphabet_file.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input("Enter a word: ").upper()
-result_list = [alphabet_dict[letter] for letter in name]
-print(result_list)
+while True:
+    try:
+        name = input("Enter a word: ").upper()
+        result_list = [alphabet_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(result_list)
 
