49f33dca39f9a131f2f32fd9e2a34674f2449050
Lawrence <lawrence891106@gmail.com>
2024-12-17 01:00:04 +0800

    After a whole day hard working, finally finish the flight deals project with sheet tracking and SMS notification function

--- Flight Deals/data_manager.py
+++ Flight Deals/data_manager.py
@@ -1,15 +1,59 @@
 import requests, os, dotenv
+from typing import List
+from requests.auth import HTTPBasicAuth
 
 dotenv.load_dotenv()
 
+# Global Constance
 SHEETY_AUTH_KEY = os.getenv("SHEETY_TOKEN")
+SHEETY_ENDPOINT = "https://api.sheety.co/f505c85ccf6b21da99164e646ed462cb/flightDeals/prices"
+
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    headers = {
-        "Authorization": SHEETY_AUTH_KEY
-    }
 
-    response = requests.get("https://api.sheety.co/854cce0fe880b4c4f9f6005261c20228/flightDeals/prices", headers=headers)
-    response.raise_for_status()
-    print(response.json())
+    def __init__(self):
+        self.destination_data = []
+        self._authorization = f"Bearer {SHEETY_AUTH_KEY}"
+
+    def get_destination_data(self) -> List:
+        response = requests.get(SHEETY_ENDPOINT, headers={"Authorization": self._authorization})
+        response.raise_for_status()
+        self.destination_data = response.json()["prices"]
+        return self.destination_data
+
+    def update_iata_code(self):
+        for city in self.destination_data:
+            try:
+                params = {
+                    'price': {
+                        'iataCode': city["iataCode"]
+                    }
+                }
+                response = requests.put(
+                    url=f"{SHEETY_ENDPOINT}/{city['id']}",
+                    headers={"Authorization": self._authorization},
+                    json=params)
+                response.raise_for_status()
+            except KeyError as K:
+                print("There are some problem about the data. Please try again", K)
+
+    def update_lowest_price(self):
+        for city in self.destination_data:
+            try:
+                params = {
+                    'price': {
+                        'lowestPrice': city["lowestPrice"]
+                    }
+                }
+                response = requests.put(
+                    url=f"{SHEETY_ENDPOINT}/{city['id']}",
+                    headers={"Authorization": self._authorization},
+                    json=params)
+                response.raise_for_status()
+            except KeyError:
+                print("There are some problem about the data. Please try again")
+
+# datamanager = DataManager()
+# datamanager.get_destination_data()
+# print(datamanager.destination_data)
--- Flight Deals/main.py
+++ Flight Deals/main.py
@@ -1,1 +1,51 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+from flight_data import find_cheapest_flight
+
+from data_manager import DataManager
+from datetime import datetime, timedelta
+import time
+
+BASE_AIRPORT = "TPE"
+
+dataManager = DataManager()
+sheet_data = dataManager.get_destination_data()
+flightSearch = FlightSearch()
+notification_manager = NotificationManager()
+
+# Check the sheet and refine it
+for row in sheet_data:
+    if row['iataCode'] == '':
+        row["iataCode"] = flightSearch.get_iata_code(row['city'])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+dataManager.destination_data = sheet_data
+dataManager.update_iata_code()
+
+# Search the lowest price
+tomorrow = datetime.today() + timedelta(days=1)
+six_month_later = tomorrow + timedelta(days=6 * 30)
+for destination in sheet_data:
+    flight_data = flightSearch.check_flight(
+        BASE_AIRPORT,
+        destination['iataCode'],
+        from_time=tomorrow,
+        to_time=six_month_later
+    )
+
+    cheapest_flight = find_cheapest_flight(flight_data)
+    print(f"{destination['city']}: NT${cheapest_flight.price}")
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        destination["lowestPrice"] = cheapest_flight.price
+    time.sleep(2)
+dataManager.destination_data = sheet_data
+dataManager.update_lowest_price()
+
+
