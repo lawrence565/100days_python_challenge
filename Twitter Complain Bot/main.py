550e782c4bad0ca1031a37b8e1b5c6dae70d809c
Lawrence <lawrence891106@gmail.com>
2024-12-21 11:10:12 +0800

    Modify the X post bot.

--- Twitter Complain Bot/main.py
+++ Twitter Complain Bot/main.py
@@ -1,16 +1,14 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from InternetSpeedTwitterBot import InternetSpeedTwitterBot
-
 
 PROMISED_DOWN = 100
 PROMISED_UP = 10
 X_DRIVER_PATH = 'https://x.com/'
 
 Bot = InternetSpeedTwitterBot()
-# Bot.get_internet_speed()
+# Bot.get_internet_speed() # The speedtest is blocking the selenium user.
 
 edge_options = webdriver.EdgeOptions()
 edge_options.add_experimental_option("detach", True)
