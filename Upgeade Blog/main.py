dc3e2c24314005e41fc0a3c0f96a05d3c4d88f29
Lawrence <lawrence891106@gmail.com>
2024-12-30 13:13:35 +0800

    Finish the day 59 blog project

--- Upgeade Blog/main.py
+++ Upgeade Blog/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import requests
 
 posts = requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
-print(posts)
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,6 +15,14 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/blog/<_id>")
+def blog(_id):
+    for post in posts:
+        if str(post['id']) == _id:
+            return render_template('post.html', post=post)
+    else:
+        return render_template("index.html", all_posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
