d9aadc1d8bbd0efc85a33f5143a175b1e5155720
Lawrence <lawrence891106@gmail.com>
2025-01-09 21:18:06 +0800

    Finishing the day 65 project with a movie ranking database

--- Web development/day-64 top-movies/main.py
+++ Web development/day-64 top-movies/main.py
@@ -1,5 +1,3 @@
-from crypt import methods
-
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -17,20 +15,23 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+ACCESS_TOKEN = getenv('ACCESS_TOKEN')
+IMG_ADDRESS = 'https://image.tmdb.org/t/p/w500'
 Bootstrap5(app)
 
 
-class MovieForm(FlaskForm):
+class AddMovieForm(FlaskForm):
     title = StringField('Movie', validators=[DataRequired()])
-    year = StringField('Year', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    rating = SelectField(label='Rating',
+    submit = SubmitField('Submit')
+
+
+class RateMovieForm(FlaskForm):
+    rating = SelectField(label='Your Rating out of five',
                          choices=[(0, 'No Star'), (1, '⭐'), (2, '⭐️⭐️'), (3, '⭐️⭐️⭐️'), (4, '⭐️⭐️⭐️⭐️'),
                                   (5, '⭐️⭐️⭐️⭐️⭐️')],
                          validators=[DataRequired()])
-    ranking = StringField('Ranking (1-10)', validators=[DataRequired()])
+    # ranking = StringField('Ranking (1-10)', validators=[DataRequired()])
     review = StringField('Review', validators=[DataRequired()])
-    img_url = StringField('Image URL', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -45,14 +46,14 @@ db.init_app(app)
 
 # CREATE TABLE
 class Movie(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(unique=True, nullable=False)
-    year: Mapped[int] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=False)
-    rating: Mapped[int] = mapped_column(nullable=False)
-    ranking: Mapped[int] = mapped_column(nullable=False)
-    review: Mapped[str] = mapped_column(nullable=False)
-    img_url: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    rating: Mapped[int] = mapped_column(Integer, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 with app.app_context():
@@ -61,53 +62,94 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    movies = db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars()
+    movies = db.session.execute(db.select(Movie).order_by(Movie.rating)).scalars().all()
+
+    for i in range(len(movies)):
+        movies[i].ranking = len(movies) - i
+    db.session.commit()
 
     return render_template("index.html", movies=movies)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add_movie():
-    form = MovieForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        new_movie = Movie(
-            title=form.title.data,
-            year=form.year.data,
-            description=form.description.data,
-            rating=form.rating.data,
-            ranking=form.ranking.data,
-            review=form.review.data,
-            img_url=form.img_url.data,
-        )
+    form = AddMovieForm()
 
-        db.session.add(new_movie)
+    if request.method == 'POST' and form.validate_on_submit():
+        title = form.title.data
+        header = {
+            "accept": "application/json",
+            "Authorization": "Bearer " + ACCESS_TOKEN,
+        }
+        params = {
+            'query': title,
+        }
+        res = requests.get('https://api.themoviedb.org/3/search/movie', params=params, headers=header)
+        return render_template('select.html', movies=res.json()['results'])
+    return render_template('add.html', form=form)
+
+
+@app.route("/select", methods=['GET'])
+def select_movie():
+    _id = request.args.get("id")
+
+    header = {
+        "accept": "application/json",
+        "Authorization": "Bearer " + ACCESS_TOKEN,
+    }
+
+    res = requests.get('https://api.themoviedb.org/3/movie/' + _id, headers=header)
+    movie = res.json()
+
+    print(movie, 'success')
+    new_movie = Movie(
+        title=movie["title"],
+        year=movie['release_date'][:4],
+        description=movie["overview"],
+        img_url=IMG_ADDRESS + movie["poster_path"],
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+
+    new_id = db.session.query(Movie).filter(Movie.title == movie['title']).first().id
+
+    return redirect(url_for('rate_movie', id = new_id))
+
+
+@app.route("/rate", methods=['GET', 'POST'])
+def rate_movie():
+    _id = request.args.get("id")
+
+    movie = db.session.execute(db.select(Movie).where(Movie.id == _id)).scalar()
+    form = RateMovieForm(obj=movie)
+
+    if request.method == 'POST' and form.validate_on_submit():
+        movie.rating = form.rating.data
+        movie.review = form.review.data
         db.session.commit()
         return redirect("/")
 
-    return render_template('add.html', form=form)
+    return render_template('edit.html', form=form)
 
-@app.route("/edit/<_id>", methods=['GET', 'POST'])
-def edit_movie(_id):
+
+@app.route("/update/<_id>", methods=['GET', 'POST'])
+def update_movie(_id):
     movie = db.session.execute(db.select(Movie).where(Movie.id == _id)).scalar()
-    form = MovieForm(obj=movie)
+    form = RateMovieForm(obj=movie)
 
     if request.method == 'POST' and form.validate_on_submit():
-        movie.title = form.title.data
-        movie.year = form.year.data
-        movie.description = form.description.data
         movie.rating = form.rating.data
-        movie.ranking = form.ranking.data
         movie.review = form.review.data
-        movie.img_url = form.img_url.data
-
         db.session.commit()  # 提交更改
         return redirect("/")
 
     return render_template('edit.html', form=form)
 
+
 @app.route("/delete/<_id>", methods=['GET'])
 def delete_movie(_id):
     db.session.execute(db.delete(Movie).where(Movie.id == _id))
+    db.session.commit()
 
     return redirect("/")
 
